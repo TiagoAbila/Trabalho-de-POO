0d6da892535d3e309a4f5956c4023509eafaf9f1
A-pulo <58056127+A-pulo@users.noreply.github.com>
2019-11-22 22:13:28 -0200

    Criado classe controladora de conexão com a database

---
+++ Trabalho/src/Controller/Sql_query.java
@@ -0,0 +1,10 @@
+package Controller;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Sql_query {
+
+}
