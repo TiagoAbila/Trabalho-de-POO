d3d2a1e53a109f7ad5b441357591a8a4b8b7c80a
Pedro M. R. Souza <49080429+Pedr0MRS@users.noreply.github.com>
2019-11-24 16:20:45 -0300

    Construtor criado

--- Trabalho/src/Controller/Leitura.java
+++ Trabalho/src/Controller/Leitura.java
@@ -1,5 +1,21 @@
 package Controller;
 
-public class Leitura {
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
+public class Leitura {
+	public Leitura(File arquivo) {
+		
+	}
 }
