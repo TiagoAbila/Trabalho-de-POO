33839cb87a56c64476098cc8bf3a4de0792cdb0f
Pedro M. R. Souza <49080429+Pedr0MRS@users.noreply.github.com>
2019-11-24 13:10:39 -0300

    Atributos na classe Material

--- Trabalho/src/Model/Material.java
+++ Trabalho/src/Model/Material.java
@@ -1,5 +1,88 @@
 package Model;
 
 public class Material {
+	private String titulo;
+	private String anoPublicacao;
+	private String urlDisponivel;
+	private int nrPaginas;
+	private String edicao;
+	private String nrISBN;
+	private String nrISSN;
+	
+	public Material() {
+		
+	}
+	
+	public Material(String titulo, String urlDisponivel) {
+		this.setTitulo(titulo);
+		this.setUrlDisponivel(urlDisponivel);
+	}
 
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAnoProducao() {
+		return anoProducao;
+	}
+
+	public void setAnoProducao(String anoProducao) {
+		this.anoProducao = anoProducao;
+	}
+
+	public String getAnoPublicacao() {
+		return anoPublicacao;
+	}
+
+	public void setAnoPublicacao(String anoPublicacao) {
+		this.anoPublicacao = anoPublicacao;
+	}
+
+	public String getUrlDisponivel() {
+		return urlDisponivel;
+	}
+
+	public void setUrlDisponivel(String urlDisponivel) {
+		this.urlDisponivel = urlDisponivel;
+	}
+
+	public int getNrPaginas() {
+		return nrPaginas;
+	}
+
+	public void setNrPaginas(int nrPaginas) {
+		this.nrPaginas = nrPaginas;
+	}
+
+	public String getEdicao() {
+		return edicao;
+	}
+
+	public void setEdicao(String edicao) {
+		this.edicao = edicao;
+	}
+
+	public String getNrISBN() {
+		return nrISBN;
+	}
+
+	public void setNrISBN(String nrISBN) {
+		this.nrISBN = nrISBN;
+	}
+
+	public String getNrISSN() {
+		return nrISSN;
+	}
+
+	public void setNrISSN(String nrISSN) {
+		this.nrISSN = nrISSN;
+	}
+	
+	
+	
+	
 }
