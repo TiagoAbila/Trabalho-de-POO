bfead12512c79199c37de00cfbc9d96f2de736be
Pedro M. R. Souza <49080429+Pedr0MRS@users.noreply.github.com>
2019-12-01 21:52:03 -0300

    Classe de Apresentação modificada pra testes

--- Trabalho/src/View/Apresentacao.java
+++ Trabalho/src/View/Apresentacao.java
@@ -24,6 +24,8 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 
 public class Apresentacao extends JFrame {	
 	private JPanel contentPane;
@@ -40,31 +42,72 @@ public class Apresentacao extends JFrame {
 
 	public Apresentacao(File arq) throws IOException {
 		this.setArquivoTrabalhado(arq);
-		Leitura contrador = new Leitura(arq);
+		Leitura leitor = new Leitura(arq);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 500);
+		setBounds(100, 100, 800, 536);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(10, 11, 764, 440);
+		tabbedPane.setBounds(10, 11, 764, 439);
 		contentPane.add(tabbedPane);
 		
-		JPanel panel = new JPanel();
-		tabbedPane.addTab(" 1 ", null, panel, null);
-		panel.setLayout(null);
+		JPanel panelTipo = new JPanel();
+		tabbedPane.addTab("Tipo de Material", null, panelTipo, null);
+		panelTipo.setLayout(null);
 		
+<<<<<<< HEAD
+		JTextArea textArea = new JTextArea();
+		textArea.setEditable(false);
+		textArea.setBounds(10, 11, 739, 355);
+		panel.add(textArea);
+		
+		JScrollPane scrPane = new JScrollPane(textArea,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scrPane.setLocation(10, 11);
+		scrPane.setSize(739, 355);
+		panel.add(scrPane);
+		
+		JButton btnTeste = new JButton("Teste");
+		btnTeste.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textArea.setText(leitor.getContadorTeste()+"\n"+leitor.teste());
+			}
+		});
+		btnTeste.setBounds(660, 378, 89, 23);
+		panel.add(btnTeste);
+		
+		
+=======
 		JTextPane textPane = new JTextPane();
 		textPane.setBounds(10, 11, 739, 351);
-		panel.add(textPane);		
+		panelTipo.add(textPane);		
 		
-		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab(" 2 ", null, panel_1, null);
+		JPanel panelMeioDiv = new JPanel();
+		tabbedPane.addTab("Meio de Divulga\u00E7\u00E3o", null, panelMeioDiv, null);
 		
-		JPanel panel_2 = new JPanel();
-		tabbedPane.addTab(" 3 ", null, panel_2, null);
+		JPanel panelEntidade = new JPanel();
+		tabbedPane.addTab("Entidade", null, panelEntidade, null);
+		
+		JPanel panelOrganizacao = new JPanel();
+		tabbedPane.addTab("Organiza\u00E7\u00E3o", null, panelOrganizacao, null);
+		
+		JPanel panelAutor = new JPanel();
+		tabbedPane.addTab("Autor", null, panelAutor, null);
+		
+		JPanel panelLocal = new JPanel();
+		tabbedPane.addTab("Local de Publica\u00E7\u00E3o", null, panelLocal, null);
+		
+		JPanel panelEditora = new JPanel();
+		tabbedPane.addTab("Editora", null, panelEditora, null);
+		
+		JPanel panelISSN = new JPanel();
+		tabbedPane.addTab("ISSN", null, panelISSN, null);
+		
+		JPanel panelPalChave = new JPanel();
+		tabbedPane.addTab("Palavras Chave", null, panelPalChave, null);
+>>>>>>> c5913d64f18f9c5696617be65b1c3594f44dcf6c
 	}
 }
